9fb400b9fce6c2c0b715a2d48cac1503b102043d
Jkungfupanda <jamesjosephpeirano@gmail.com>
2022-08-05 20:31:15 -0400

    added login functionality

--- woohoomaxbot/woohoomaxbot.py
+++ woohoomaxbot/woohoomaxbot.py
@@ -5,21 +5,43 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import date
 import os
-
+from selenium.webdriver.common.keys import Keys
 
 PATH = "C:/Users/james/Desktop/msedgedriver.exe"  #will be wherever you have your microsoft edge driver downloaded
 driver = webdriver.Edge(PATH)
 
+email = "email@email12.com"  #put your email address in here
+passwrd = "asdfasdfa"  #put your password in here
 
-url = "https://filesamples.com/formats/xls#google_vignette" #input the the url where you are going to download the excel
+url = "https://www.facebook.com/" #input the the url where you are going to download the excel file
 
 driver.get(url)
+
+
+try:
+    element = WebDriverWait(driver, 10).until(
+         EC.presence_of_element_located( (By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input" ) ) and   #paste the FULL xpath of the login box
+        EC.presence_of_element_located( (By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input") )  #paste the FULL xpath of the password box
+       
+    )
+    login = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input")  #paste the FULLxpath of the login box
+    login.send_keys(email)
+    password = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input") #paste the FULL xpath of the password box
+    password.send_keys(passwrd)
+    password.send_keys(Keys.RETURN)  #this clicks enter after it has entered your email and password for you
+
+except:
+    print("could not click login or password button")
+    time.sleep(10)
+
+time.sleep(5) #wait to login
+
 
 
 # first checkbox
 # try:
 #     element=WebDriverWait(driver,10).until(
-#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))     #paste the xpath of the first checkbox
+#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))     #paste the FULL xpath of the first checkbox
 #    )
 #     element.click()
 # except:
@@ -30,7 +52,7 @@ driver.get(url)
 # #second checkbox
 # try:
 #     element=WebDriverWait(driver,10).until(
-#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))    #paste the xpath of the 2nd checkbox
+#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))    #paste the FULL xpath of the 2nd checkbox
 #    )
 #     element.click()
 # except:
@@ -41,7 +63,7 @@ driver.get(url)
 # #third checkbox
 # try:
 #     element=WebDriverWait(driver,10).until(
-#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))    #paste the xpath of the 3rd checkbox
+#        EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/main/div[2]/div[1]/a"))    #paste the FULL xpath of the 3rd checkbox
 #    )
 #     element.click()
 # except:
@@ -53,7 +75,7 @@ driver.get(url)
 try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div[1]/main/div[2]/div[1]/a")    #paste the xpath of the button to download the excel file
+            (By.XPATH, "/html/body/div[1]/main/div[2]/div[1]/a")    #paste the FULL xpath of the button to download the excel file
         )
     )
     element.click()
